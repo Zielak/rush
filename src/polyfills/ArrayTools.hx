fc79e1654c433525005a1e4a188c2e6b39672cfc
Darek Zieliński <dariusz.zielinski@performgroup.com>
2018-01-18 12:07:56 +0100

    more unit tests and Array tools

--- src/polyfills/ArrayTools.hx
+++ src/polyfills/ArrayTools.hx
@@ -2,10 +2,49 @@ package polyfills;
 
 class ArrayTools {
   // TODO: not sure if its correct
-  static public function fill(arr:Array<Dynamic>, predicate:(Dynamic -> Int -> Array<Dynamic> -> Dynamic)):Array<Dynamic> {
+  static public function fill<T>(arr:Array<T>, value:Dynamic, ?start:Int = 0, ?end:Int):Array<T> {
+    // Steps 1-2.
+    // if (this == null) {
+    //   throw new TypeError('this is null or not defined');
+    // }
+    var newArr:Array<T> = arr.copy();
+
+    // var O = Object(this);
+
+    // Steps 3-5.
+    var len = arr.length >>> 0;
+
+    // Steps 6-7.
+    var relativeStart = start >> 0;
+
+    // Step 8.
+    var k:Int = (relativeStart < 0) ?
+      Std.int(Math.max(len + relativeStart, 0)) :
+      Std.int(Math.min(relativeStart, len));
+
+    // Steps 9-10.
+    var relativeEnd = end == null ?
+      len : end >> 0;
+
+    // Step 11.
+    var fin = relativeEnd < 0 ?
+      Math.max(len + relativeEnd, 0) :
+      Math.min(relativeEnd, len);
+
+    // Step 12.
+    while (k < fin) {
+      newArr[k] = value;
+      k++;
+    }
+
+    // Step 13.
+    return newArr;
+  }
+
+  static public function find<T>(arr:Array<T>, predicate:(T -> ?Int -> ?Array<T> -> Bool)):T {
     // 1. Let O be ? ToObject(this value).
     // if (this == null) {
-    //   throw new TypeError('"this" is null or not defined');
+    //   throw '"this" is null or not defined';
     // }
 
     // var o = Object(this);
@@ -41,44 +80,16 @@ class ArrayTools {
     // 7. Return undefined.
     return null;
   }
+  
+  static public function some<T>(arr:Array<T>, fun:(T -> ?Int -> ?Array<T> -> Bool)):Bool {
+    var len = arr.length >>> 0;
 
-  static public function find<T>(arr:Array<T>, predicate:(T -> ?Int -> ?Array<T> -> Bool)):T {
-    // 1. Let O be ? ToObject(this value).
-    // if (this == null) {
-    //   throw '"this" is null or not defined';
-    // }
-
-    // var o = Object(this);
-
-    // 2. Let len be ? ToLength(? Get(O, "length")).
-    var len:Int = arr.length >>> 0;
-
-    // 3. If IsCallable(predicate) is false, throw a TypeError exception.
-    if (!Reflect.isFunction(predicate)) {
-      throw 'predicate must be a function';
-    }
-
-    // 4. If thisArg was supplied, let T be thisArg; else let T be undefined.
-    // var thisArg = arguments[1];
-
-    // 5. Let k be 0.
-    var k = 0;
-
-    // 6. Repeat, while k < len
-    while (k < len) {
-      // a. Let Pk be ! ToString(k).
-      // b. Let kValue be ? Get(O, Pk).
-      // c. Let testResult be ToBoolean(? Call(predicate, T, « kValue, k, O »)).
-      // d. If testResult is true, return kValue.
-      var kValue:Dynamic = arr[k];
-      if (predicate(kValue, k, arr)) {
-        return kValue;
+    for (i in 0...len) {
+      if (fun(arr[i], i, arr)) {
+        return true;
       }
-      // e. Increase k by 1.
-      k++;
     }
 
-    // 7. Return undefined.
-    return null;
+    return false;
   }
 }
--- test/polyfills/TestArrayTools.hx
+++ test/polyfills/TestArrayTools.hx
@@ -7,13 +7,121 @@ using polyfills.ArrayTools;
 class TestArrayTools {
   public function new(){}
   
-  public function testFind() {
-    trace('hello, testFind()');
+  public function testFindString() {
     // Find the string with 3 digits
     var arr:Array<String> = ['4444', '22', '7777777', '333', '55555'];
     var found = arr.find(function(curr:String, ?_, ?_){
       return curr.length == 3;
     });
     Assert.equals('333', found);
+  }
+  public function testFindFloat() {
+    // Find the string with 3 digits
+    var arr:Array<Float> = [-5.1, -0.1, 0, 354.89];
+    var found:Float;
+    
+    found = arr.find(function(curr:Float, ?_, ?_){
+      return curr > 0;
+    });
+    Assert.equals(354.89, found);
+    
+    found = arr.find(function(curr:Float, ?_, ?_){
+      return curr < 0;
+    });
+    Assert.equals(-5.1, found);
+  }
+  public function testFindIdx() {
+    // IDX
+    var arr:Array<Int> = [-100, 0, 50, 250];
+    var i:Int = 0;
+    arr.find(function(curr:Float, ?idx:Int, ?_){
+      Assert.equals(i, idx, 'i = ${i}, idx = ${idx}');
+      i++;
+      return false;
+    });
+  }
+  
+  /**
+   *  FILL
+   */
+  public function testFillString() {
+    var arr:Array<String> = ['a', 'b', 'c', 'd'];
+    var newArr:Array<String>;
+    
+    newArr = arr.fill('q');
+    Assert.equals(['q', 'q', 'q', 'q'].toString(), newArr.toString());
+    
+    newArr = arr.fill('123');
+    Assert.equals(['123', '123', '123', '123'].toString(), newArr.toString());
+    
+    newArr = arr.fill('q', 0, 4);
+    Assert.equals(['q', 'q', 'q', 'q'].toString(), newArr.toString());
+    
+    newArr = arr.fill('q', 4, 4);
+    Assert.equals(['a', 'b', 'c', 'd'].toString(), newArr.toString());
+    
+    newArr = arr.fill('q', 1);
+    Assert.equals(['a', 'q', 'q', 'q'].toString(), newArr.toString());
+    
+    newArr = arr.fill('q', 2);
+    Assert.equals(['a', 'b', 'q', 'q'].toString(), newArr.toString());
+    
+    newArr = arr.fill('q', 0, 2);
+    Assert.equals(['q', 'q', 'c', 'd'].toString(), newArr.toString());
+  }
+  public function testFillInt() {
+    var arr:Array<Int> = [123, -1234, 0, -55];
+    var newArr:Array<Int>;
+    
+    newArr = arr.fill(0);
+    Assert.equals([0, 0, 0, 0].join(','), newArr.join(','));
+    newArr = arr.fill(0, 1);
+    Assert.equals([123, 0, 0, 0].join(','), newArr.join(','));
+    newArr = arr.fill(0, 0, 4);
+    Assert.equals([0, 0, 0, 0].join(','), newArr.join(','));
+    newArr = arr.fill(0, 1, 3);
+    Assert.equals([123, 0, 0, -55].join(','), newArr.join(','));
+    
+    newArr = arr.fill(2314);
+    Assert.equals([2314, 2314, 2314, 2314].join(','), newArr.join(','));
+    
+    newArr = arr.fill(-12);
+    Assert.equals([-12, -12, -12, -12].join(','), newArr.join(','));
+  }
+  public function testFillFloat() {
+    var arr:Array<Float> = [0.03, -3.149865, 0, -555];
+    var newArr:Array<Float>;
+    
+    newArr = arr.fill(0);
+    Assert.equals([0, 0, 0, 0].join(','), newArr.join(','));
+    
+    newArr = arr.fill(2314);
+    Assert.equals([2314, 2314, 2314, 2314].join(','), newArr.join(','));
+    
+    newArr = arr.fill(-12);
+    Assert.equals([-12, -12, -12, -12].join(','), newArr.join(','));
+  }
+  
+  /**
+   *  SOME
+   */
+  public function testSomeString() {
+    var arr1:Array<String> = ['asd', 'rest245ef', 'snkdjfn', '94873289559'];
+    var arr2:Array<String> = ['a', '2', 'r5', 's'];
+    var arr3:Array<String> = ['aaa', 'bbb', 'ccc', 'ddd'];
+    
+    function hasMoreThan10Chars(elem:String, ?_, ?_):Bool {
+      return elem.length > 10;
+    }
+    function containsNumbers(elem:String, ?_, ?_):Bool {
+      return Std.is(Std.parseInt(elem), Float);
+    }
+    
+    Assert.isTrue(arr1.some(hasMoreThan10Chars));
+    Assert.isFalse(arr2.some(hasMoreThan10Chars));
+    
+    Assert.isTrue(arr1.some(containsNumbers));
+    Assert.isTrue(arr2.some(containsNumbers));
+    Assert.isFalse(arr3.some(containsNumbers));
   }
 }
