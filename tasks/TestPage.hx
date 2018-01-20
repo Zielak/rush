2863df27858f23c466a9b3e6b59a76cce44f714f
Darek Greenly <git@zielak.pl>
2018-01-20 16:06:30 +0100

    testpage on windows fix

--- tasks/TestPage.hx
+++ tasks/TestPage.hx
@@ -4,12 +4,14 @@ class TestPage {
   static function main() {
     var cmdArgs = Sys.args();
     var debug:Bool = (cmdArgs[0] == '--debug');
+    // on windows it still tries to connect to 9001
+    var port:String = Sys.systemName() == 'Windows' ? '9001' : '8099';
 
     var args:Array<String> = [
       '--interval', '50',
       '-s', 'bin/web/',
       '-sp', '8090',
-      '-p', '8099', // TODO: on windows it still tries to connect to 9001
+      '-p', port,
       'src/', '-e', 'flow build web${debug ? ' --debug' : ''}',
       'assets/', '-e', 'flow build web${debug ? ' --debug' : ''}'
     ];
